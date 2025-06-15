2a2b2a8b6bd2bcbb516803d12017cfcafb13f728
nigel523 <nigel@safehouseservices.co.uk>
2025-06-15 17:17:13 +0300

    Update app-plugins.js

--- app-plugins.js
+++ app-plugins.js
@@ -1,1 +1,16 @@
+const fp = require("fastify-plugin");
+const cookie = require("@fastify/cookie");
+const session = require("@fastify/session");
 
+async function sessionPlugins(fastify, options) {
+  await fastify.register(cookie);
+  await fastify.register(session, {
+    secret: process.env.SESSION_SECRET,
+    cookie: { secure: true, maxAge: 86400000 },
+    saveUninitialized: false,
+    resave: false,
+  });
+}
+
+// Export the plugin, marking it as a self-contained unit
+module.exports = fp(sessionPlugins);
